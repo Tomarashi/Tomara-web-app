b87cfc335ae9bbe141f8e963be71e2b7894df3a6
Giorgi Megrelishvili <gmegr18@freeuni.edu.ge>
2023-04-16 00:46:40 +0400

    Added dependencies to gradle. Removed SpringBootTest from tests

--- src/test/kotlin/ge/tomara/Tomarawebapp/TomaraWebAppApplicationTests.kt
+++ src/test/kotlin/ge/tomara/Tomarawebapp/TomaraWebAppApplicationTests.kt
@@ -1,9 +1,7 @@
 package ge.tomara.Tomarawebapp
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class TomaraWebAppApplicationTests {
 
 	@Test
