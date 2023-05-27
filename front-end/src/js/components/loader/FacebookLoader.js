0f2e2e779b47045d53ffb265a133749a1624bbb0
Giorgi Megrelishvili <gmegr18@freeuni.edu.ge>
2023-05-28 01:46:11 +0400

    Created WordsEdit component

---
+++ front-end/src/js/components/loader/FacebookLoader.js
@@ -0,0 +1,12 @@
+import "../../../css/facebook-loader.css";
+
+const FacebookLoader = function(props) {
+    const className = ["facebook-type-loader", props["className"] || ""];
+    return (
+        <div className={className.join(" ")}>
+            <div /><div /><div />
+        </div>
+    );
+};
+
+export default FacebookLoader;
