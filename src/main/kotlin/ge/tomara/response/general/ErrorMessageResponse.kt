c397e28c9384645d0d25232cdd6d81f2fdd36315
Giorgi Megrelishvili <gmegr18@freeuni.edu.ge>
2023-06-29 17:53:18 +0400

    Modified general responses

--- src/main/kotlin/ge/tomara/response/general/ErrorMessageResponse.kt
+++ src/main/kotlin/ge/tomara/response/general/ErrorMessageResponse.kt
@@ -1,7 +1,21 @@
 package ge.tomara.response.general
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 
-data class ErrorMessageResponse(
-    @JsonProperty("error_msg") val errorMsg: String,
-)
+class ErrorMessageResponse(errorCode: Int, private val errorMsg: String) {
+    private var errorCode: Int? = errorCode
+
+    constructor(errorMsg: String): this(0, errorMsg) {
+        errorCode = null
+    }
+
+    @JsonValue
+    fun value(): Map<String, Any> {
+        return HashMap<String, Any>().apply {
+            put("error_msg", errorMsg)
+            if(errorCode != null) {
+                put("error_code", errorCode!!)
+            }
+        }
+    }
+}
--- src/main/kotlin/ge/tomara/response/general/SuccessMessageResponse.kt
+++ src/main/kotlin/ge/tomara/response/general/SuccessMessageResponse.kt
@@ -1,7 +1,21 @@
 package ge.tomara.response.general
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 
-data class SuccessMessageResponse(
-    @JsonProperty("success_msg") val successMsg: String,
-)
+class SuccessMessageResponse(successCode: Int, private val successMsg: String) {
+    private var successCode: Int? = successCode
+
+    constructor(successMsg: String): this(0, successMsg) {
+        successCode = null
+    }
+
+    @JsonValue
+    fun value(): Map<String, Any> {
+        return HashMap<String, Any>().apply {
+            put("success_msg", successMsg)
+            if(successCode != null) {
+                put("success_code", successCode!!)
+            }
+        }
+    }
+}
